7863b956d10bd5971e5cc38963452b2a47016316
lsb <likith15600@gmail.com>
2025-01-31 19:28:01 +0530

    Added services card modal effect

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,49 +1,121 @@
-import { Box, VStack, Card, SimpleGrid } from "@chakra-ui/react";
+import { Box, Image, Button, Text } from "@chakra-ui/react";
+import email from "../assets/images/Formal_Email.png";
+import script from "../assets/images/apprameya_proj_4.jpg";
+import ads from "../assets/images/apprameya_proj_5.jpg";
+import caption from "../assets/images/apprameya_proj_6.jpg";
+import description from "../assets/images/apprameya_proj_7.png";
+import {
+  DialogActionTrigger,
+  DialogBody,
+  DialogCloseTrigger,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
+
+interface ServiceCardProps {
+  img: string;
+  title: string;
+  desc: String;
+}
 
 const Services = () => {
+  const cards: ServiceCardProps[] = [
+    {
+      img: email,
+      title: "Email writing",
+      desc: "We craft compelling email copies that not only promote your product or service but also build a strong connection with your audience.",
+    },
+    {
+      img: script,
+      title: "Script writing",
+      desc: "From short films to YouTube videos and feature-length movies, we create impactful scripts and engaging stories that bring your vision to life.",
+    },
+    {
+      img: caption,
+      title: "Caption writing",
+      desc: "Our team writes creative and brand-aligned captions for Instagram ads, helping you stand out in the crowd.",
+    },
+    {
+      img: description,
+      title: "Description writing",
+      desc: "We craft SEO-friendly YouTube descriptions that boost discoverability and clearly convey the value of your content.",
+    },
+    {
+      img: ads,
+      title: "Ads writing",
+      desc: "We deliver persuasive ad content for promotional campaigns, posters, and marketing materials designed to capture attention and drive action.",
+    },
+    {
+      img: caption,
+      title: "Caption writing",
+      desc: "Our team writes creative and brand-aligned captions for Instagram ads, helping you stand out in the crowd.",
+    },
+  ];
+
+  const handleOpenClick = () => {
+    document.body.classList.add("active");
+  };
+
+  const handleCloseClick = () => {
+    document.body.classList.remove("active");
+  };
+
   return (
-    <Box>
-      <VStack className="services-heading">Services</VStack>
-      <SimpleGrid
-        columns={{ lg: 3, md: 2, base: 1 }}
-        className="services-card-holder"
-        gap={6}
-        p={2}
-      >
-        <Card.Root className="services-card">
-          <Card.Header textAlign="center" className="services-card-header">
-            Service 1
-          </Card.Header>
-          <Card.Description className="services-card-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            nesciunt fugit tenetur numquam perferendis ducimus, ea dolorum
-            magni, quaerat nulla animi quibusdam voluptates doloremque amet
-            deleniti rem officia architecto ad.
-          </Card.Description>
-        </Card.Root>
-        <Card.Root className="services-card">
-          <Card.Header textAlign="center" className="services-card-header">
-            Service 2
-          </Card.Header>
-          <Card.Description className="services-card-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            nesciunt fugit tenetur numquam perferendis ducimus, ea dolorum
-            magni, quaerat nulla animi quibusdam voluptates doloremque amet
-            deleniti rem officia architecto ad.
-          </Card.Description>
-        </Card.Root>
-        <Card.Root className="services-card">
-          <Card.Header textAlign="center" className="services-card-header">
-            Service 3
-          </Card.Header>
-          <Card.Description className="services-card-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
-            nesciunt fugit tenetur numquam perferendis ducimus, ea dolorum
-            magni, quaerat nulla animi quibusdam voluptates doloremque amet
-            deleniti rem officia architecto ad.
-          </Card.Description>
-        </Card.Root>
-      </SimpleGrid>
+    <Box className="services">
+      <Box textAlign="center">
+        <Text className="services-text">Services</Text>
+      </Box>
+      <Box className="services-card-container" textAlign="center">
+        {cards.map((card, index) => (
+          <Box className="services-card">
+            <DialogRoot
+              placement="center"
+              motionPreset="slide-in-bottom"
+              closeOnInteractOutside={false}
+            >
+              <DialogTrigger>
+                <Button className="service-card-desc" onClick={handleOpenClick}>
+                  {card.title}
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="services-dialog">
+                <DialogHeader>
+                  <DialogTitle>
+                    <Text fontWeight="700" className="service-dialog-title">
+                      {card.title}
+                    </Text>
+                  </DialogTitle>
+                </DialogHeader>
+                <DialogBody>
+                  <Text
+                    className="service-dialog-desc"
+                    wordSpacing={2}
+                    letterSpacing={1}
+                  >
+                    {card.desc}
+                  </Text>
+                </DialogBody>
+                <DialogFooter>
+                  <DialogActionTrigger>
+                    <Button onClick={handleCloseClick}>Close</Button>
+                  </DialogActionTrigger>
+                </DialogFooter>
+                <DialogCloseTrigger onClick={handleCloseClick} />
+              </DialogContent>
+            </DialogRoot>
+            <Image
+              className="services-card-img"
+              src={card.img}
+              objectFit="cover"
+              key={index}
+            />
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 };
