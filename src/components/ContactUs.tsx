7f9bf7abe445626f563f5c29b5adea55de2ccc05
lsb <likith15600@gmail.com>
2025-02-08 14:36:34 +0530

    Made contact section responsive to semi smaller screens

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,7 +2,6 @@ import {
   Box,
   Button,
   Card,
-  defineStyle,
   Field,
   GridItem,
   HStack,
@@ -16,30 +15,6 @@ import {
 import { Tooltip } from "../components/ui/tooltip";
 
 const ContactUs = () => {
-  const floatingStyles = defineStyle({
-    pos: "absolute",
-    bg: "#F3DEC5",
-    px: "0.5",
-    top: "-3",
-    insetStart: "2",
-    fontWeight: "normal",
-    pointerEvents: "none",
-    transition: "position",
-    borderRadius: "4px",
-    fontSize: "17px",
-    padding: "4px",
-    _peerPlaceholderShown: {
-      color: "black",
-      top: "2.5",
-      insetStart: "3",
-    },
-    _peerFocusVisible: {
-      color: "black",
-      top: "-3",
-      insetStart: "2",
-    },
-  });
-
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
@@ -96,74 +71,51 @@ const ContactUs = () => {
             </Box>
           </VStack>
         </GridItem>
-        <GridItem>
-          <Card.Root className="contact-card" maxW="400px" ml={20}>
+        <GridItem className="contact-card-grid">
+          <Card.Root className="contact-card">
             <Card.Body>
               <form onSubmit={handleSubmit}>
                 <Field.Root mb={5}>
                   <Box pos="relative" w="full">
                     <Input
                       className="peer cont-input"
-                      placeholder=""
+                      placeholder="Enter your Name"
                       type="text"
                       name="name"
                       required
                     />
-                    <Field.Label
-                      css={floatingStyles}
-                      className="cont-card-label1"
-                    >
-                      Name
-                    </Field.Label>
                   </Box>
                 </Field.Root>
                 <Field.Root mb={5}>
                   <Box pos="relative" w="full">
                     <Input
                       className="peer cont-input"
-                      placeholder=""
+                      placeholder="Enter your Email"
                       type="email"
                       name="email"
                       required
                     />
-                    <Field.Label
-                      css={floatingStyles}
-                      className="cont-card-label1"
-                    >
-                      Email
-                    </Field.Label>
                   </Box>
                 </Field.Root>
                 <Field.Root mb={5}>
                   <Box pos="relative" w="full">
                     <Input
                       className="peer cont-input"
-                      placeholder=""
+                      placeholder="Enter the subject"
                       type="text"
                       name="subject"
                     />
-                    <Field.Label
-                      css={floatingStyles}
-                      className="cont-card-label1"
-                    >
-                      Subject
-                    </Field.Label>
                   </Box>
                 </Field.Root>
                 <Field.Root mb={5}>
                   <Box pos="relative" w="full">
                     <Textarea
                       className="peer cont-input"
-                      placeholder=""
+                      placeholder="Enter your message..."
                       name="message"
                       resize="none"
+                      required
                     />
-                    <Field.Label
-                      css={floatingStyles}
-                      className="cont-card-label1"
-                    >
-                      Message
-                    </Field.Label>
                   </Box>
                 </Field.Root>
                 <Button className="cont-submit" type="submit">
@@ -174,7 +126,7 @@ const ContactUs = () => {
           </Card.Root>
         </GridItem>
       </SimpleGrid>
-      <HStack className="contact-social-butt" justifyContent="center" gap={70}>
+      <HStack className="contact-social-butt" justifyContent="center">
         <Button className="butt">Twitter</Button>
         <Button className="butt">Instagram</Button>
         <Button className="butt">LinkedIn</Button>
