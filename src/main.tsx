380317ea5921a15e4985a647718d8b475ae0ce68
lsb <likith15600@gmail.com>
2025-03-22 12:43:48 +0530

    Refactored code

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./App.css";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
-// import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
